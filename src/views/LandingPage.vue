cdd90a7e364091d38f6940e502c7dd5cfe67d1bb
Jun Lee <jun@Juns-MacBook-Pro.local>
2023-04-08 14:23:40 -0500

    landing page updated

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -29,71 +29,71 @@
     <v-row justify="center">
       <v-card v-for="n in 3" :key="n" class="cardClass" outlined tile>
         <div v-if="n === 1">
-          <v-img class="imageClass" @click="dialog = true" src="./images/artWork1.jpeg"></v-img>
+          <v-img class="imageClass" @click="dialog = true" src="./images/1_Eden.png"></v-img>
         </div>
         <div v-else-if="n === 2">
-          <v-img @click="dialog2 = true" src="./images/artWork7.png" width="300" height="425"></v-img>
+          <v-img @click="dialog2 = true" src="./images/2_Ustaarabu.png" width="300" height="425"></v-img>
         </div>
         <div v-else-if="n === 3">
-          <v-img @click="dialog3 = true" src="./images/artWork2.png" width="300" height="425"></v-img>
+          <v-img @click="dialog3 = true" src="./images/3_.png" width="300" height="425"></v-img>
         </div>
         <br>
         <div v-if="n === 1" class="workName">
-          Toad House (2018)
+          Eden
         </div>
         <div v-if="n === 2" class="workName">
-          νίκη (2017)
+          Ustaarabu
         </div>
         <div v-if="n === 3" class="workName">
-          Wrath (2018)
+          極樂
         </div>
       </v-card>
 
       <v-card v-for="n in 3" :key="n" class="cardClass" outlined tile>
         <div v-if="n === 1">
-          <v-img @click="dialog4 = true" src="./images/artWork5.png" width="300" height="425"></v-img>
+          <v-img @click="dialog4 = true" src="./images/4_Haka.png" width="300" height="425"></v-img>
         </div>
         <div v-else-if="n === 2">
-          <v-img @click="dialog5 = true" src="./images/artWork3.png" width="300" height="425"></v-img>
+          <v-img @click="dialog5 = true" src="./images/5_Toad_s House.png" width="300" height="425"></v-img>
         </div>
         <div v-else-if="n === 3">
-          <v-img @click="dialog6 = true" src="./images/artWork8.png" width="300" height="425"></v-img>
+          <v-img @click="dialog6 = true" src="./images/6_UngNyeo.png" width="300" height="425"></v-img>
         </div>
         <br>
         <div v-if="n === 1" class="workName">
-          Under Pressure (2017)
+          Haka
         </div>
         <div v-if="n === 2" class="workName">
-          Fragment (2018)
+          Toad’s House
         </div>
         <div v-if="n === 3" class="workName">
-          Jesus (2017)
+          Ung-Nyeo
         </div>
       </v-card>
 
-      <v-card v-for="n in 3" :key="n" class="cardClass" outlined tile>
+      <v-card v-for="n in 1" :key="n" class="cardClass" outlined tile>
         <div v-if="n === 1">
-          <v-img @click="dialog7 = true" src="./images/artWork9.png" width="300" height="425"></v-img>
+          <v-img @click="dialog7 = true" src="./images/7_6th World.png" width="300" height="425"></v-img>
         </div>
-        <div v-else-if="n === 2">
+        <!-- <div v-else-if="n === 2">
           <v-img @click="dialog8 = true" src="./images/artWork10.png" width="300" height="425"></v-img>
         </div>
         <div v-else-if="n === 3">
           <v-img @click="dialog9 = true" src="./images/artWork11.png" width="300" height="425"></v-img>
-        </div>
+        </div> -->
         <br>
         <div v-if="n === 1" class="workName">
-          Third Adam pt1. (2017)
+          6th World
         </div>
-        <div v-if="n === 2" class="workName">
-          Third Adam pt2. (2017)
+        <!-- <div v-if="n === 2" class="workName">
+          Third Adam pt2.
         </div>
         <div v-if="n === 3" class="workName">
-          Third Adam pt3. (2017)
-        </div>
+          Third Adam pt3.
+        </div> -->
       </v-card>
 
-      <v-card v-for="n in 3" :key="n" class="cardClass" outlined tile>
+      <!-- <v-card v-for="n in 3" :key="n" class="cardClass" outlined tile>
         <div v-if="n === 1">
           <v-img @click="dialog10 = true" src="./images/artWork13.png" width="300" height="425"></v-img>
         </div>
@@ -105,52 +105,143 @@
         </div>
         <br>
         <div v-if="n === 1" class="workName">
-          Beauty (2019)
+          Beauty
         </div>
         <div v-if="n === 2" class="workName">
-          Seasons (2019)
+          Seasons
         </div>
         <div v-if="n === 3" class="workName">
-          Kiss (2016)
+          Kiss
         </div>
-      </v-card>
+      </v-card> -->
     </v-row>
 
     <!-- dialog (modal) -->
     <div class="prevent">
-      <v-dialog v-model="dialog" width="600">
+      <v-dialog v-model="dialog" width="850">
         <v-card>
-          <v-img @click="dialog = false" src="./images/artWork1.jpeg"></v-img>
+          <v-img @click="dialog = false" src="./images/1_Eden.png"></v-img>
+          <v-card-title class="text-h5">
+            Eden
+          </v-card-title>
+          <v-card-text>
+            Imagination of our future often gets to put into the margin between Utopia and Dystopia. I firmly believe that
+            the key factor of dividing these two contrasting idea is an existence of individual freedom in the society, as
+            we already experienced numerous
+            historical tragedy as an outcome of absence of freedom under the authoritarian beliefs. Therefore, I imagine
+            Utopia as a place where each individual’s idea to be placed in more accepted manner, where different ideas do
+            not clash but
+            harmonize with another instead.
+          </v-card-text>
         </v-card>
       </v-dialog>
-      <v-dialog v-model="dialog2" width="600">
+      <v-dialog v-model="dialog2" width="850">
         <v-card>
-          <v-img @click="dialog2 = false" src="./images/artWork7.png"></v-img>
+          <v-img @click="dialog2 = false" src="./images/2_Ustaarabu.png"></v-img>
+          <v-card-title class="text-h5">
+            Ustaarabu (civilization)
+          </v-card-title>
+          <v-card-text>
+            Humans started to rapidly dominate the natural world enhancing their ability to build a community in
+            incomparable scale than the other animals, and religions often was the main catalyst for their source of
+            unity. In the early stage of human civilization,
+            religion was an efficient methods to share the same perception of the world from different individuals, and
+            even formed sense of morality later becoming a foundation for laws.
+          </v-card-text>
         </v-card>
       </v-dialog>
-      <v-dialog v-model="dialog3" width="600">
+      <v-dialog v-model="dialog3" width="850">
         <v-card>
-          <v-img @click="dialog3 = false" src="./images/artWork2.png"></v-img>
+          <v-img @click="dialog3 = false" src="./images/3_.png"></v-img>
+          <v-card-title class="text-h5">
+            極樂
+          </v-card-title>
+          <v-card-text>
+            Buddhist idea of 極樂 (Guk-Lak) have two separated branch. First is 極樂淨土 (Guk-Lak-Jung-To), an idea that is
+            similar to the Abraham branch religion’s heaven, where you must constantly work in order to achieve
+            enlightenment to enter the heaven, while the
+            other idea known as 唯心淨土 (Yu-Sim-Jung-To) suggests that heaven is already inside everyone’s heart, and we must
+            get rid of all the greeds and worries and accept that our life itself already is a heaven. Throughout the
+            history of mankind,
+            humans have developed the society by sacrificing individual’s happiness in order to achieve each society’s
+            perception of utopia. Perhaps in this time of rapid technological progress, we need to question ourselves if
+            are these technological
+            advancements are necessary to shape our society into a heaven.
+          </v-card-text>
         </v-card>
       </v-dialog>
-      <v-dialog v-model="dialog4" width="600">
+      <v-dialog v-model="dialog4" width="850">
         <v-card>
-          <v-img @click="dialog4 = false" src="./images/artWork5.png"></v-img>
+          <v-img @click="dialog4 = false" src="./images/4_Haka.png"></v-img>
+          <v-card-title class="text-h5">
+            Haka
+          </v-card-title>
+          <v-card-text>
+            Civilization arose in the Pacific islands known as Polynesian has remarkable trait of sharing relatively the
+            similar religious beliefs, culture, and language along the 1,000 islands in Pacific Ocean. Maori tribe is one
+            of the most well-known tribe among
+            them, famous for their ritual/performance at the beginning of the battle known as “Haka”, where warriors of
+            the Maori tribe horrendous expressions with their face and movement. Haka is such a fascinating war tactics,
+            as people believed
+            that by performing the Haka, warriors absorbs the enemies “Mana”(spirit) from their fear, and as numbers of
+            Polynesian society shared similar belief, Warriors of Maori were able to win numerous battles without shedding
+            any bloods nor
+            killing any people.
+          </v-card-text>
         </v-card>
       </v-dialog>
       <v-dialog v-model="dialog5" width="600">
         <v-card>
-          <v-img @click="dialog5 = false" src="./images/artWork3.png"></v-img>
+          <v-img @click="dialog5 = false" src="./images/5_Toad_s House.png"></v-img>
+          <v-card-title class="text-h5">
+            Toad’s House
+          </v-card-title>
+          <v-card-text>
+            In Korea, toads often symbolize the wisdom, rich, and stability. And if you take a brief look at the history
+            of Korea, it is full of continuous internal changes brought by the invaders from different countries due to
+            its geographical location. And often times it was the common people who were suffered and sacrificed from
+            these drastic changes within the society.
+
+            Originally, Toad’s house is a traditional children’s song and play from Korea, where kids sing “Toad, Toad, I
+            will give my old house, so give me a new house” while digging a tunnel in the ground. Perhaps this absurd
+            lyrics represent my ancestor’s desire to live in a stability after facing numerous shifts.
+          </v-card-text>
         </v-card>
       </v-dialog>
       <v-dialog v-model="dialog6" width="600">
         <v-card>
-          <v-img @click="dialog6 = false" src="./images/artWork8.png"></v-img>
+          <v-img @click="dialog6 = false" src="./images/6_UngNyeo.png"></v-img>
+          <v-card-title class="text-h5">
+            Ung-Nyeo
+          </v-card-title>
+          <v-card-text>
+            Ung-Nyeo is a character from the ancient Korean mythology, DanGun myth. In this ancient mythology, there were
+            once a bear and a tiger went in a journey to find god(Ok-Hwang-Sang-Jeh) with a desire of becoming human. And
+            after they met god, god gave them a test where both bear and tiger had to stay inside the cave just eating
+            garlics and mugwort for 100 days, in return of granting their wish to become a human. At the end of the test,
+            bear was the only one who succeeded the task, granted her wish to become a human and became the first queen of
+            Korea.
+
+            Recent discoveries from China recorded a war from the Manchurian region by the tribe who worships bears, and
+            tribe who worships tiger. If you read the mythology once again, it suggests that tribe of bears represented by
+            Ung Nyeo perhaps won from this war, and became the foundation of Korean people up to this date.
+          </v-card-text>
         </v-card>
       </v-dialog>
-      <v-dialog v-model="dialog7" width="800">
+      <v-dialog v-model="dialog7" width="600">
         <v-card>
-          <v-img @click="dialog7 = false" src="./images/artWork9.png"></v-img>
+          <v-img @click="dialog7 = false" src="./images/7_6th World.png"></v-img>
+          <v-card-title class="text-h5">
+            6th World
+          </v-card-title>
+          <v-card-text>
+            In one of the most popular variation of Aztec mythology, people of Aztec believed our universe repeats from
+            the process of creation and destruction, and the current universe we’re living in today is the 5th universe
+            known as universe of movement. In order to create this universe of movement, Tonatiuh, ugliest god who
+            willingly sacrificed himself in fire to become a sun, and each gods sacrificed their heart to the sacred snake
+            covered with feathers known as Quetzalcóatl, where he provided movement to the Sun, moon and stars and gave
+            cycle of life.
+          </v-card-text>
         </v-card>
       </v-dialog>
       <v-dialog v-model="dialog8" width="800">
@@ -241,9 +332,9 @@ export default {
 .workName {
   text-align: center;
   font-family: 'Times New Roman', Times, serif;
-  font-size: 15px;
-  font-style: italic;
+  font-size: 17px;
   font-weight: bold;
+  margin-top: -8px;
 }
 
 .darkModeSwitch {
@@ -279,5 +370,4 @@ export default {
     margin: 70px;
     padding: 10px;
   }
-}
-</style>
+}</style>
